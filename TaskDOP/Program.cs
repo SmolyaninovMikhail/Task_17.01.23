d055ee0b7842319aea4b8cf51d92f5ec8da6837b
Mike <big.ben@gmail.com>
2023-01-21 22:12:28 +0300

    добавил доп задачу

---
+++ TaskDOP/Program.cs
@@ -0,0 +1,34 @@
+﻿// Task dop
+
+Console.Clear();
+int n = Convert.ToInt32(Console.ReadLine());
+int maxFirst = n;
+int maxSecond = 0;
+while (n != 0)
+{
+n = Convert.ToInt32(Console.ReadLine());
+if (n > maxFirst)
+{
+    maxSecond = maxFirst;
+    maxFirst = n;
+}
+else if (n > maxSecond)
+    maxSecond = n;
+}
+Console.WriteLine(maxSecond);
+
+
+// Console.Clear();
+// int n = Convert.ToInt32(Console.ReadLine()), max1 = n, max2 = 0;
+// while (n != 0)
+// {
+// n = Convert.ToInt32(Console.ReadLine());
+// if (n > max1)
+// {
+// max2 = max1;
+// max1 = n;
+// }
+// else if (n > max2)
+// max2 = n;
+// }
+// Console.WriteLine(max2);
